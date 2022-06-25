4fc3a5d07bf316c430aaee632753160ef086c06e
Louis Hassett <louishassett@outlook.com>
2022-06-25 17:29:32 +0100

    variables moved below functions for efficiency

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,39 +1,4 @@
 // use strict
-
-const quizContainer = document.getElementById('quiz');
-const resultContainer = document.getElementById('results');
-const submitButton = document.getElementById('submit');
-
-const myQuestions = [
-    {
-        question: "What year did World War 2 begin?",
-        answers: {
-            a: "1939",
-            b: "1914",
-            c: "1972"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "Who wrote the famous play Romeo & Juliet?",
-        answers: {
-            a: "Donald Trump",
-            b: "William Shakespeare",
-            c: "J.K.Rowling"
-        },
-        correctAnswer: "b"
-    },
-    {
-        question: "What is the human body mostly made up of?",
-        answers: {
-            a: "Syrup",
-            b: "Milk",
-            c: "Water"
-        },
-        correctAnswer: "c"
-    },
-];
-
 function buildQuiz(){
     // variable to store the HTML output
     const output = [];
@@ -70,6 +35,40 @@ function buildQuiz(){
 }
 
 function showResults(){}
+
+const quizContainer = document.getElementById('quiz');
+const resultContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+const myQuestions = [
+    {
+        question: "What year did World War 2 begin?",
+        answers: {
+            a: "1939",
+            b: "1914",
+            c: "1972"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who wrote the famous play Romeo & Juliet?",
+        answers: {
+            a: "Donald Trump",
+            b: "William Shakespeare",
+            c: "J.K.Rowling"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the human body mostly made up of?",
+        answers: {
+            a: "Syrup",
+            b: "Milk",
+            c: "Water"
+        },
+        correctAnswer: "c"
+    },
+];
 
 //display quiz right away
 buildQuiz();
