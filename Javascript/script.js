aaa004b44587ed74ee9e9903c5947b2735528b60
Louis Hassett <louishassett@outlook.com>
2022-06-25 17:14:26 +0100

    3 questions including answers and correct answer added

--- Javascript/script.js
+++ Javascript/script.js
@@ -5,8 +5,34 @@ const resultContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
 
 const myQuestions = [
-    
-]
+    {
+        question: "What year did World War 2 begin?",
+        answers: {
+            a: "1939",
+            b: "1914",
+            c: "1972"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Who wrote the famous play Romeo & Juliet?",
+        answers: {
+            a: "Donald Trump",
+            b: "William Shakespeare",
+            c: "J.K.Rowling"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What is the human body mostly made up of?",
+        answers: {
+            a: "Syrup",
+            b: "Milk",
+            c: "Water"
+        },
+        correctAnswer: "c"
+    },
+];
 
 function buildQuiz(){}
 
